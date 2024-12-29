9d2a7678e75212fe964a0381c0ad25a0b6004b1a
Khang <bogiakuro1245@gmail.com>
2024-12-29 07:26:51 +0700

    Maybe final ver

--- common/include/constants.h
+++ common/include/constants.h
@@ -20,6 +20,9 @@
 #define MAX_QUESTIONS 1000
 #define NUM_QUESTIONS_PER_EXAM 10
 #define EXAM_TIME_LIMIT 3600  // 1 hour in seconds
+#define MAX_QUESTION_RETRIES 3  // Số lần được phép thay đổi đáp án
+#define REVIEW_TIME_LIMIT 300   // Thời gian cho phép xem lại (5 phút)
+#define MAX_EXAM_DURATION 7200  // Thời gian tối đa cho một bài thi (2 giờ)
 
 // Practice settings
 #define MAX_PRACTICE_QUESTIONS 100
@@ -41,9 +44,39 @@
 #define ROOM_WAITING 0
 #define ROOM_IN_PROGRESS 1
 #define ROOM_COMPLETED 2
+#define ROOM_REVIEWING 3   // Trạng thái phòng đang trong chế độ xem lại
 
 // Answer settings
 #define MAX_ANSWER_LENGTH 1
 #define VALID_ANSWERS "ABCD"
+
+// Review mode settings
+#define REVIEW_ENABLED 1
+#define REVIEW_DISABLED 0
+#define MAX_REVIEWS_PER_QUESTION 3  // Số lần được xem lại mỗi câu hỏi
+
+// Format settings
+#define MIN_QUESTIONS 10            // Số câu hỏi tối thiểu
+#define MAX_QUESTIONS_PER_EXAM 60   // Số câu hỏi tối đa
+#define MIN_TIME_LIMIT 30           // Thời gian thi tối thiểu (phút)
+#define MAX_TIME_LIMIT 180          // Thời gian thi tối đa (phút)
+#define MAX_DIFFICULTY_RATIO 100    // Tỷ lệ độ khó tối đa (%)
+
+// Score settings
+#define SCORE_PER_EASY 1       // Điểm cho câu dễ
+#define SCORE_PER_MEDIUM 2     // Điểm cho câu trung bình
+#define SCORE_PER_HARD 3       // Điểm cho câu khó
+#define PENALTY_FOR_CHANGE 0.5  // Điểm trừ khi thay đổi đáp án
+
+// Timeout settings
+#define ANSWER_TIMEOUT 300     // Thời gian tối đa cho mỗi câu (giây)
+#define REVIEW_TIMEOUT 60      // Thời gian tối đa xem lại mỗi câu (giây)
+#define CHANGE_TIMEOUT 120     // Thời gian tối đa để thay đổi đáp án (giây)
+
+// Debug levels
+#define DEBUG_NONE 0
+#define DEBUG_ERROR 1
+#define DEBUG_INFO 2
+#define DEBUG_VERBOSE 3
 
 #endif // CONSTANTS_H
--- common/include/protocol.h
+++ common/include/protocol.h
@@ -2,12 +2,12 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
-// Authentication commands
+// Authentication commands 
 #define CMD_REGISTER "REGISTER"
 #define CMD_LOGIN "LOGIN"
 #define CMD_LOGOUT "LOGOUT"
 
-// Room management commands
+// Room management commands 
 #define CMD_CREATE_ROOM "CREATE_ROOM"
 #define CMD_JOIN_ROOM "JOIN_ROOM"
 #define CMD_LEAVE_ROOM "LEAVE_ROOM"
@@ -15,13 +15,18 @@
 #define CMD_DELETE_ROOM "DELETE_ROOM"
 #define CMD_START_EXAM "START_EXAM"
 
-// Exam commands
+// Exam commands 
 #define CMD_SUBMIT_ANSWER "SUBMIT_ANSWER"
 #define CMD_SUBMIT_EARLY "SUBMIT_EARLY"
 #define CMD_GET_SCORE "GET_SCORE"
 #define CMD_GET_RESULTS "GET_RESULTS"
+#define CMD_REVIEW_QUESTION "REVIEW"         
+#define CMD_CHANGE_EXAM_ANSWER "CHANGE"      
+#define CMD_GET_EXAM_TIME "TIME"           
+#define CMD_LIST_ANSWERS "LIST_ANSWERS"     
+#define CMD_SET_EXAM_FORMAT "SET_EXAM_FORMAT"  
 
-// Practice mode commands
+// Practice mode commands 
 #define CMD_START_PRACTICE "START_PRACTICE"
 #define CMD_LEAVE_PRACTICE "LEAVE_PRACTICE"
 #define CMD_GET_SUBJECTS "GET_SUBJECTS"
@@ -30,7 +35,7 @@
 #define CMD_SUBMIT_PRACTICE "SUBMIT"
 #define CMD_CHANGE_ANSWER "CHANGE_ANSWER"
 
-// Server responses
+// Server responses 
 #define RESP_SUCCESS "SUCCESS"
 #define RESP_AUTH_FAILED "AUTH_FAILED"
 #define RESP_NOT_FOUND "NOT_FOUND"
@@ -44,8 +49,11 @@
 #define RESP_PRACTICE_ACCEPT "PRACTICE_ACCEPT"
 #define RESP_TIMEOUT "TIMEOUT"
 #define RESP_SUBJECTS "SUBJECTS"
+#define RESP_ANSWER_CHANGED "ANSWER_CHANGED"    
+#define RESP_QUESTION_LIST "QUESTION_LIST"      
+#define RESP_EXAM_FORMAT_SET "FORMAT_ACCEPTED"  
 
-// Response codes
+// Response codes 
 #define RESP_CODE_SUCCESS 200
 #define RESP_CODE_AUTH_FAILED 401
 #define RESP_CODE_NOT_FOUND 404
@@ -54,7 +62,7 @@
 #define RESP_CODE_INVALID_REQUEST 400
 #define RESP_CODE_FORBIDDEN 403
 
-// Format errors
+// Format errors 
 #define ERROR_FORMAT "ERROR_FORMAT"
 #define ERROR_NO_QUESTIONS "NO_QUESTIONS_AVAILABLE"
 #define ERROR_TIME_EXPIRED "TIME_EXPIRED"
@@ -63,16 +71,22 @@
 #define ERROR_ROOM_EXISTS "ROOM_EXISTS"
 #define ERROR_USER_EXISTS "USER_EXISTS"
 #define ERROR_INVALID_CREDENTIALS "INVALID_CREDENTIALS"
+#define ERROR_QUESTION_NOT_FOUND "QUESTION_NOT_FOUND"  
+#define ERROR_CANNOT_CHANGE "CANNOT_CHANGE_ANSWER"     
+#define ERROR_INVALID_QUESTION "INVALID_QUESTION_NUMBER" 
 
-// Question format
+// Question format 
 #define MAX_QUESTION_LENGTH 200
 #define MAX_OPTION_LENGTH 100
 #define MAX_SUBJECT_LENGTH 50
 
-// Response format prefixes
+// Response format prefixes 
 #define PREFIX_SCORE "SCORE:"
 #define PREFIX_TIME_LEFT "TIME_LEFT:"
 #define PREFIX_QUESTION "Question"
 #define PREFIX_SUBJECTS "SUBJECTS|"
+#define PREFIX_ANSWER_LIST "ANSWERS:"       
+#define PREFIX_QUESTION_REVIEW "REVIEW:"    
+#define PREFIX_CHANGE_RESULT "CHANGED:"     
 
 #endif // PROTOCOL_H
